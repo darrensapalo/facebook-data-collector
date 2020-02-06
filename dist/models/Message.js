77fdc49c51f05acdeab2e34e24eed1eb42cb3c02
Darren Karl A. Sapalo <darren.sapalo@gmail.com>
2020-02-06 22:00:00 +0800

    Fixed gitignore.

--- dist/models/Message.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var verbose = true;
-var Message = /** @class */ (function () {
-    function Message() {
-    }
-    return Message;
-}());
-exports.default = Message;
--- dist/util/API.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.API = {
-    AirTable: {
-        LectorsAndCommentators: "https://api.airtable.com/v0/appyuLP8Stp9omHWY/Lectors%20and%20Commentators"
-    }
-};
-exports.default = exports.API;
